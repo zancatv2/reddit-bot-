de6e8a9e82d935b5d1a6f384d4593dea6df2b8d6
zancatv2 <eronlatif@gmail.com>
2025-01-13 20:07:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,74 @@
-import praw
+import  praw 
+import syllables
+from gtts import gTTS
+from moviepy.config import change_settings
+change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})
+
+from moviepy.editor import *
+import os
+
+                 
 
 reddit = praw.Reddit(
     client_id="rAIDYUguYJ4f-b17HpRLHg",
     client_secret="",
-    user_agent="skib skib",
+    user_agent="my user agent",
 )
 
 
+#global variables
 x = 0
+str_post = ""
+clip = VideoFileClip("skib.mp4") 
+start_time = 0 
+clip_num = 0  
 
 
-#goes to r/askreddit and saves the title in title.txxt
+#takes top submission and top 5 comments and appends to a string
+for submission in reddit.subreddit("ohio").hot(limit=1):
 
-for submission in reddit.subreddit("askreddit").hot(limit=1):
-    print(submission.title)
+    str_post += (submission.title) 
 
-    with open("title.txt","w") as f:
-        f.write(submission.title)
+    for comment in submission.comments:
 
+        str_post += (comment.body + " ")
+        
+        x += 1 
+        if x == 5:
+            x = 0 
+            break
+
+#splits string into list of words
+str_post = str_post.replace("."," ")
+arr_post = str_post.split()
+
+
+def create_video(word, start_time, syl, clip_num):
+    
+    print(clip_num)
+    arr_post[clip_num] = TextClip(word , fontsize = 100, color = 'white', stroke_color = 'black', stroke_width= 3)  
+    arr_post[clip_num] = arr_post[clip_num].set_pos('center').set_start(start_time).set_duration(syl)  
+    
+  
     
 
+for word in arr_post:
+    
+    syl = syllables.estimate(word)*0.26
+    create_video(word,start_time,syl,clip_num)
+    start_time += syl
+    clip_num += 1
 
 
-    with open("comments.txt","w") as f:
-            
-            submission.comments(limit=5)
-            for top_level_comment in submission.comments:
-                f.write(top_level_comment.body)
-                
+
+tts = gTTS(text=str_post, lang='en')
+tts.save('output.mp3')
+
+
+audio = AudioFileClip("output.mp3")
+video = CompositeVideoClip([clip, *arr_post]).set_audio(audio)
+
+video.write_videofile("dopdop.mp4", codec="libx264")
+
+
+
